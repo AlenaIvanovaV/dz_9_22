182ed0fa9259e4c266ec9b00689e57b69039e503
ivanovaalena <angel40995@inbox.ru>
2023-02-19 20:58:17 +0300

    test

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -2,20 +2,33 @@
 from selene.support.shared import browser
 from qa_test.models.data.studentform import*
 from qa_test.models.pages import practice_form
-
-
-
+import allure
+from allure_commons._allure import attach
+from qa_test.utils import attach
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 def test_fill_form(open_browser):
-
-
-
-
+  options = Options()
+  selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+  options.capabilities.update(selenoid_capabilities)
+  driver = webdriver.Remote(
+         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+         options=options)
+ browser.config.driver = driver
+  with allure.step('Открыть форму регистрации студента'):
     browser.open('/automation-practice-form')
-
+  with allure.step('Заполняем данные'):
     practice_form.fill_data(aivanova)
-
+  with allure.step('Сохраняем данные'):
     practice_form.submit()
-
+  with allure.step('Проверяем данные'):
     practice_form.submitted(
               [
                   ('Student Name', 'Alena Ivanova'),
@@ -30,3 +43,7 @@ def test_fill_form(open_browser):
                   ('State', 'NCR Delhi'),
               ],
           )
+  attach.add_html(browser)
+  attach.add_screenshot(browser)
+  attach.add_logs(browser)
+  attach.add_video(browser)
